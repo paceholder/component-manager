a7df6389cd98e654d094632b72b3b8430b740f10
Dmitry Pinaev <dimitry.pinaev@gmail.com>
2017-05-02 14:05:25 +0200

    Add more export statements

--- source/ModuleLoader.hpp
+++ source/ModuleLoader.hpp
@@ -4,12 +4,16 @@
 
 #include <QtCore/QStringList>
 
+#include "Export.hpp"
+
 namespace ComponentManager
 {
 
+CM_PUBLIC
 void
 loadModules(std::vector<QString> const &moduleJsonFiles);
 
+CM_PUBLIC
 void
 loadModule(QString const &moduleJsonFile);
 
